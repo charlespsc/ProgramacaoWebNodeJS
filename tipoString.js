660636c85aa8e175bc88a322170ee4745238daef
Charles Pereira <charlespereira.fln@gmail.com>
2022-03-08 11:01:53 -0300

    string

---
+++ tipoString.js
@@ -0,0 +1,17 @@
+const nome = 'Charles'
+const sobrenome = 'Pereira'
+console.log('Prof. ' + nome + sobrenome)
+
+const idade = '44'
+console.log('Prof. ' + nome + ' tem ' + idade + ' anos!')
+
+let str = 'Teste'
+console.log(str.length) // retorna a quantidade de caracteres
+console.log(str.toUpperCase()) // retorna tudo em maiúscula
+console.log(str.toLowerCase())
+str = '1'
+console.log(parseInt(str))
+str = '1.5'
+console.log(parseFloat(str))
+
+
