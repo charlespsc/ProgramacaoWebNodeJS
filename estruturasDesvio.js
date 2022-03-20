2ec576305735a73a3882f8e9e41cd8cc755ffba5
Charles Pereira <charlespereira.fln@gmail.com>
2022-03-20 01:47:13 -0300

    DESV

---
+++ estruturasDesvio.js
@@ -0,0 +1,21 @@
+let x1 = 0
+if(x1 === 0)
+    console.log('X1 é zero!')
+
+let x2 = 0
+if(x2 === 0){
+    console.log('X1 é zero!')
+    x2++
+}
+
+let x3 = 0
+if(x3 % 2 === 0){
+    console.log('X3 é PAR!')
+}
+
+for(let i= 1; i <= 20; i++){
+    if(i % 2 === 0){
+        console.log(i + ' é PAR!')
+    }
+}
+
