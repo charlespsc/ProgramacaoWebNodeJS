436f10eeb70e5d94e4ebdc0c9aa82e26414213d3
Charles Pereira <charlespereira.fln@gmail.com>
2022-03-20 01:23:23 -0300

    rep

---
+++ estruturaRepeticao.js
@@ -0,0 +1,58 @@
+for (var i = 0; i < 10; i++)
+    console.log(`i 1 = ` + i)
+
+
+for (var i = 0; i < 10; i++){
+    console.log("i 2 = " + i)
+    // outra instrução qualquer, quantas quiser
+}
+
+let chave = true
+while(chave === true){
+    // outra instrução qualquer 
+    console.log('Imprime 1')
+    chave = false
+}
+
+let I = 0
+while(I < 10){
+    // outra instrução qualquer 
+    console.log('Valor 2')
+    I++
+}
+
+let Chave = false
+do{
+    // outra instrução qualquer
+    console.log('Imprime 3')
+}while(Chave === true)
+
+let x = 0
+console.log(x++)
+console.log(x++)
+console.log(x++)
+console.log(x++)
+console.log(x++)
+console.log(x++)
+console.log(x++)
+console.log(x++)
+console.log(x++)
+console.log(x++)
+
+for(let i=0; i < 10; i++)
+    console.log(i)
+
+let XX = 0
+while(XX < 12)
+    console.log(XX++)
+
+let x1 = 0
+do{
+    console.log(x1)
+}while(x1++ < 3)
+
+let x2 = 0
+console.log(x2++)
+
+let x3 = 0
+console.log(++x3)
