d7e3fbf2f2cb88212d3983da080bdf241fc158ca
Charles Pereira <charlespereira.fln@gmail.com>
2022-03-21 00:05:11 -0300

    arrays

---
+++ arrays.js
@@ -0,0 +1,8 @@
+const inventores = []
+inventores[0] = 'Einstein'
+inventores[1] = 'Edson'
+inventores[2] = 'Galileu'
+inventores[3] = 'Da Vinci'
+
+const inventor = inventores[3]
+console.log(inventor)
